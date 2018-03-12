72bccf16bd2b37639647abc0d6e683196abbb0ce
Lorraine Philllips <ljcphilllips@gmail.com>
2018-03-12 20:15:59 +0000

    student directory using methods

--- directory.rb
+++ directory.rb
@@ -1,6 +1,5 @@
 #let's put all students into an array
 students = [
-"-------------",
 "Dr. Hannibal Lecter",
 "Darth Vader",
 "Nurse Ratched",
@@ -13,11 +12,19 @@ students = [
 "Joffrey Baratheon",
 "Norman Bates"
 ]
-#and then print them
-puts "The students of Villains Academy"
-puts "-------------"
-students.each do |student|
-  puts student
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
 end
-#finally, we print the total
-puts "Overall, we have #{students.count} great students"
+def print(names)
+  names.each do |name|
+    puts name
+  end
+end
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+#nothing happens until we call the methods
+print_header
+print(students)
+print_footer(students)
