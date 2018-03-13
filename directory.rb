4ec58b1443c1238ea3400941fcb6baf446bed942
Lorraine Philllips <ljcphilllips@gmail.com>
2018-03-13 18:26:58 +0000

    student directory specific letter

--- directory.rb
+++ directory.rb
@@ -38,7 +38,10 @@ def print_header
 end
 def print(students)
   students.each_with_index do |student, index|
-    puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)"
+    if student[:name][0] == "T"
+      puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)"
+    else
+    end
   end
 end
 def print_footer(names)
